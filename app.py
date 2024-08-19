187393eb00f42a7eb9842464bd54f3127e6aea31
erin deji <erin.deji@gmail.com>
2024-08-19 12:03:28 +0100

    add packages finish app requirements

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import os
+import marqo
+from index_data import setupApp
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env.local")
+
+app = Flask(__name__)
+
+URL = "http://localhost:8882"
+CLIENT = marqo.Client(url=URL)
+INDEX_NAME = os.getenv("MARQO_INDEX_NAME", None)
+
+DEVICE = os.getenv("MARQO_DEVICE", "cpu")
+print("==== {CLIENT} {INDEX_NAME} {URL}")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        query = request.form["search"]
+        return redirect(url_for("search_results", query=query))
+    return render_template("index.html")
+
+
+@app.route("/search_results", methods=["POST"])
+def search_results():
+    data = request.get_json()
+    search_query = data["search"]
+    themes = data["themes"]
+    negs = data["negatives"]
+
+    query_weights = {
+        search_query: 1.0,
+    }
+
+    if themes:
+        query_weights[themes] = 0.75
+
+    if negs:
+        query_weights[negs] = -1.1
+
+    results = CLIENT.index(INDEX_NAME).search(query_weights, device=DEVICE, limit=30)
+
+    print(f"Found {len(results['hits'])} results in {results['processingTimeMs']} ms.")
+
+    hits = results["hits"]
+
+    return jsonify(hits)
+
+
+if __name__ == "__main__":
+    indexes = {index["indexName"] for index in CLIENT.get_indexes()["results"]}
+    if INDEX_NAME not in indexes:
+        setupApp()
+    app.run(debug=True)
